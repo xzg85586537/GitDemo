70185bbf039a649addd4e24da16955bed032f1f1
xiaozhenguo <136325220@qq.com>
2020-11-01 16:56:38 +0800

    分支合并

--- src/main/java/com/cn/demo/Test.java
+++ src/main/java/com/cn/demo/Test.java
@@ -10,6 +10,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("分支第一次提交");
+        System.out.println("分支合并");
     }
 
 }

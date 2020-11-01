3b4582d243720e5520b703504a0e6555b72db8a8
xiaozhenguo <136325220@qq.com>
2020-11-01 17:05:13 +0800

    测试提交的代码

--- src/main/java/com/cn/demo/Test.java
+++ src/main/java/com/cn/demo/Test.java
@@ -10,6 +10,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("分支第一次提交");
+
+
+        System.out.println("测试提交的代码");
     }
 
 }

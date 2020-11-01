df57333f986bfbb8a47421a7323fb22c1394524c
xiaozhenguo <136325220@qq.com>
2020-11-01 16:33:21 +0800

    master-第一个提交

---
+++ src/main/java/com/cn/demo/Test.java
@@ -0,0 +1,14 @@
+package com.cn.demo;
+
+/**
+ * @Description
+ * @Author xiao
+ * @Date 2020-11-01 16:19
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+
+}
